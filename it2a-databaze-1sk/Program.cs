9eac8b9444864607d6c532087c39d12e937c2346
Kuba49 <132440970+Kuba49@users.noreply.github.com>
2026-03-12 13:23:58 +0100

    Nahrán prázdný projekt

---
+++ it2a-databaze-1sk/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace it2a_databaze_1sk
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
